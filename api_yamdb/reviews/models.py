04cf195c0514861cbaca6ca76a6c1b647a4beea4
Maxim_Mazanko <iluckcode@gmail.com>
2026-03-31 17:43:18 +0300

    add only models names

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# start
+
+class Category(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
+
+
+class Title(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+class User(models.Model):
+    pass
