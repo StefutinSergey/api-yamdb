7966d113cc30f635624f0f8768dd0203d0518d2e
Sergey Stefutin <sergeistefutin@yandex.ru>
2026-03-31 17:51:59 +0300

    Сериалайзер. Коммент

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator 
+
+from reviews.models import Comment, Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        read_only_fields = ['review']
+        model = Comment
+        validators = ( 
+            UniqueTogetherValidator( 
+                queryset=Comment.objects.all(),
+                fields=('author', 'title_id'),
+                message='You already reviewed this title.',
+            ),
+        )
