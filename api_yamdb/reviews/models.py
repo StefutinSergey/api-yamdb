4bb4168fd29654005a52660091a1514cf47d0061
Maxim_Mazanko <iluckcode@gmail.com>
2026-04-01 03:37:48 +0300

    add models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,25 +1,63 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
-class Category(models.Model):
-    pass
+class RepresentModel(models.Model):
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
 
 
-class Genre(models.Model):
-    pass
+class SlugCheckModel(RepresentModel):
+    # Список моделей для проверки кросс-уникальности slug,
+    # исключая модель-наследника
+    slug_models = []
+
+    def clean(self):
+        """
+        Кросс-модельная проверка уникальности поля slug.
+
+        Сохранение встроенных проверок Django перед дополнительной валидацией.
+        Циклическая проверка уникальности slug по моделям, использующим поле
+        slug.
+        """
+        super().clean()
+        for model_name in self.slug_models:
+            model_class = models.apps.get_model(
+                app_label="reviews", model_name=model_name
+            )
+            if model_class.objects.filter(slug=model_class.slug).exists():
+                raise ValidationError(
+                    f"Slug уже используется в {model_class.__name__}",
+                )
+
+        class Meta:
+            abstract = True
 
 
-class Title(models.Model):
-    pass
+class NameSlugModel(SlugCheckModel):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        abstract = True
 
 
-class Review(models.Model):
-    pass
+class Category(NameSlugModel):
+    slug_models = ["Genre"]
 
 
-class Comment(models.Model):
-    pass
+class Genre(NameSlugModel):
+    slug_models = ["Category"]
 
 
-class User(models.Model):
-    pass
+class Title(RepresentModel):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, related_name="titles"
+    )
+    genre = models.ManyToManyField(Genre, related_name="titles")
