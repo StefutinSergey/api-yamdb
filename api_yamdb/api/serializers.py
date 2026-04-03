0d3e20c47ff2c09dccde7c8db12944d558b27c33
Yura Zvonarev <zvonarov.2003@mail.ru>
2026-04-03 14:30:27 +0300

    feat: добавил валидацию длины email и допустимых символов username при регистрации

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,17 +1,29 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+import re
 
 User = get_user_model()
 
 
 class SignUpSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=150)
-    email = serializers.EmailField()
+    email = serializers.EmailField(max_length=254)
 
     def validate_username(self, value):
         if value.lower() == 'me':
             raise serializers.ValidationError(
                 'Имя пользователя "me" не разрешено.')
+        if not re.match(r'^[\w.@+-]+\Z', value):
+            raise serializers.ValidationError(
+                'Имя пользователя может содержать только буквы, цифры и символы'
+            )
+        return value
+
+    def validate_email(self, value):
+        if len(value) > 254:
+            raise serializers.ValidationError(
+                'Email не может быть длинее 254 символов.'
+            )
         return value
 
 
